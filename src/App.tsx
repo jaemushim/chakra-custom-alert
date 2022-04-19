3ee149543e3cab7ec691ec2b083e2f684f15faf3
jaemushim <shimwoan@gmail.com>
2022-04-20 00:36:56 +0900

    remove default initial setting file

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,12 @@ import {
   theme,
   Button,
 } from "@chakra-ui/react";
-import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import { Alert, Confirm } from "./CustomAlert";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
           <Button
             onClick={() =>
